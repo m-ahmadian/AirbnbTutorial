d6b1e8027013261dee611b99c1d4a84e055a184f
m-ahmadian <mehrdad.ahmadian@gmail.com>
2024-03-25 23:32:14 -0400

    Add Listing Detail View - Part 1

---
+++ AirbnbTutorial/Core/Components/ListingImageCarouselView.swift
@@ -0,0 +1,32 @@
+//
+//  ListingImageCarouselView.swift
+//  AirbnbTutorial
+//
+//  Created by Mehrdad Behrouz Ahmadian on 2024-03-25.
+//
+
+import SwiftUI
+
+struct ListingImageCarouselView: View {
+    var images = [
+        "listing-1",
+        "listing-2",
+        "listing-3",
+        "listing-4"
+    ]
+    
+    var body: some View {
+        TabView {
+            ForEach(images, id: \.self) { image in
+                Image(image)
+                    .resizable()
+                    .scaledToFill()
+            }
+        }
+        .tabViewStyle(.page)
+    }
+}
+
+#Preview {
+    ListingImageCarouselView()
+}
--- AirbnbTutorial/Core/Explore/ExploreView.swift
+++ AirbnbTutorial/Core/Explore/ExploreView.swift
@@ -10,19 +10,24 @@ import SwiftUI
 struct ExploreView: View {
     var body: some View {
         NavigationStack {
-                ScrollView {
-                    SearchAndFilterBar()
-                    
-                    LazyVStack(spacing: 32) {
-                        ForEach(0...10, id: \.self) { listing in
+            ScrollView {
+                SearchAndFilterBar()
+                
+                LazyVStack(spacing: 32) {
+                    ForEach(0...10, id: \.self) { listing in
+                        NavigationLink(value: listing) {
                             ListingItemView()
                                 .frame(height: 400)
                                 .clipShape(RoundedRectangle(cornerRadius: 10))
-                            
                         }
+                        
                     }
-                    .padding()
                 }
+                .padding()
+            }
+            .navigationDestination(for: Int.self) { listing in
+                Text("Listing detail view ...")
+            }
         }
     }
 }
